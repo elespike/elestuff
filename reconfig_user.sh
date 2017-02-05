e70b34b6df2ca370bfae99df70b1b53125c4e20e
MZN Lab <elespike@mznlab.net>
2017-02-05 16:56:39 -0500

    Update reconfig_user.sh

--- reconfig_user.sh
+++ reconfig_user.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
-if [[ -n "$1" && -n "$2" ]]
+if [[ -n "${1}" && -n "${2}" ]]
 then
     echo "Renaming primary group..."
-    groupmod -n "$2" "$1"
+    groupmod -n "${2}" "${1}"
 
     echo "Renaming user..."
-    usermod -l "$2" "$1"
+    usermod -l "${2}" "${1}"
 
     echo "Updating description..."
-    usermod -c "$2" "$2"
+    usermod -c "${2}" "${2}"
 
-    if [[ -d /home/"$1" ]]
+    if [[ -d /home/"${1}" ]]
     then
         echo "Renaming home folder..."
-        mv /home/"$1" /home/"$2"
+        mv /home/"${1}" /home/"${2}"
     fi
 
     echo "Assigning home folder..."
-    usermod -d /home/"$2" "$2"
+    usermod -d /home/"${2}" "${2}"
 
     echo 'Done!'
 
 else
-    echo "Usage: "$0" CURRENT_USERNAME DESIRED_USERNAME"
+    echo "Usage: "${0}" CURRENT_USERNAME DESIRED_USERNAME"
 fi
