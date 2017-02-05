178c52e66da01bff9d44ee86322ae9b9c8a0ca39
MZN Lab <elespike@mznlab.net>
2017-02-05 16:42:52 -0500

    Update chmac.sh

--- chmac.sh
+++ chmac.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+ip link show dev ${1} &> /dev/null
+if [[ ${?} != 0 ]]
+then
+    echo "Usage: chmac.sh <ifname>"
+    exit
+fi
+
 function get_byte {
     echo $(head /dev/urandom | tr -dc a-f0-9 | head -c 2)
 }
@@ -12,22 +19,18 @@ mac=""
 
 for i in {1..3}
 do
-    mac="$mac$(get_even_byte):"
+    mac="${mac}$(get_even_byte):"
 done
 
 for i in {1..2}
 do
-    mac="$mac$(get_byte):"
+    mac="${mac}$(get_byte):"
 done
 
-mac="$mac$(get_byte)"
+mac="${mac}$(get_byte)"
 
-if [[ -n "$1" && -n "$(ip link show dev $1)" ]]
-then
-    ip link set dev "$1" down &&
-    ip link set dev "$1" address $mac &&
-    ip link set dev "$1" up &&
-    echo "Changed $1's MAC to $mac!"
-else
-    echo "Invalid or no interface specified!"
-fi
+ip link set dev "${1}" down
+[[ ${?} == 0 ]] && ip link set dev "${1}" address ${mac}
+[[ ${?} == 0 ]] && ip link set dev "${1}" up
+[[ ${?} == 0 ]] && echo "Changed ${1}'s MAC to ${mac}!"
+[[ ${?} == 0 ]] || echo "Error changing MAC on ${1}!"
