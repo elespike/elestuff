87bfb8d7d2d9632a341ce18682e775782e8aeb4e
MZN Lab <elespike@mznlab.net>
2017-04-27 16:08:26 -0400

    Overhaul

--- logspike.py
+++ logspike.py
@@ -1,23 +1,44 @@
-from logging import \
-    INFO, \
-    basicConfig, \
-    debug, \
-    error, \
-    exception, \
-    getLogger, \
-    info, \
-    warning
+from logging import (
+    Formatter    ,
+    StreamHandler,
+    addLevelName ,
+    getLevelName ,
+    getLogger    ,
+)
+from random import randint
 from sys import stdout
 
-
 class Logspike:
-    def __init__(self):
-        basicConfig(
-            level=INFO,
-            format='\r\n%(asctime)s:%(msecs)03d %(levelname)s: %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S',
-            stream=stdout)
+    STATUS = 9001
+    while 'Level' not in getLevelName(STATUS):
+        STATUS = randint(100, 10000)
+    def __init__(self, level=20):
+        self.logger = getLogger(__name__)
 
-    @staticmethod
-    def set_logging_level(level):
-        getLogger().setLevel(level)
+        self.verbosity_translator = {
+            1: 40, # ERROR
+            2: 30, # WARNING
+            3: 20, # INFO
+            4: 10, # DEBUG
+        }
+
+        self.initialize_logger(level)
+
+    def initialize_logger(self, level):
+        fmt='%(asctime)s:%(msecs)03d [%(levelname)s] %(message)s\n'
+        datefmt='%H:%M:%S'
+        formatter = Formatter(fmt=fmt, datefmt=datefmt)
+
+        handler = StreamHandler(stream=stdout)
+        handler.setFormatter(formatter)
+
+        self.logger.addHandler(handler)
+        addLevelName(Logspike.STATUS, 'STATUS')
+        self.logger.setLevel(level)
+
+    def set_logging_level(self, level):
+        if level in self.verbosity_translator:
+            newlvl = self.verbosity_translator[level]
+            self.logger.setLevel(newlvl)
+        self.logger.log(Logspike.STATUS, 'Verbosity set to {}'.format(getLevelName(newlvl)))
+
